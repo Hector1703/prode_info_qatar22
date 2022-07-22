17d03a9a31ca2bd5160a1f73fc388ca79388eb69
Hector1703 <hclarre@hotmail.com>
2022-07-22 09:20:46 -0300

    prueba

---
+++ prode/prode/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def inicio(request):
+    templete_name="inicio.html"
+    usuario={
+        "nombre": "Hector",
+        "apellido": "Larre"
+    }
+    
+    ctx={
+        "user_dict": usuario
+    }
+    return render (request, template_name, ctx)
